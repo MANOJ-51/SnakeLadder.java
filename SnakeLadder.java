fd0a5e6d1d86a27c71661dfa5302b6d586f1c417
MANOJ-51 <m4anoj51@gmail.com>
2022-05-24 20:28:19 +0530

    modified

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,9 +3,11 @@ public class SnakeLadder {
         System.out.println("WELCOME TO SNAKE & LADDER");
         int player_position = 0;
         int present_position = 0;
+        int die_roll=0;
         final int Ladder = 1;
         final int snake = 2;
         while (true) {
+          die_roll++;
             int die = (int) Math.floor(Math.random() * 6 + 1);
             System.out.println("DIE ROLLED BY PLAYER AND VALUE IS:" + die);
             if (player_position == 1) {
@@ -28,14 +30,12 @@ public class SnakeLadder {
                         System.out.println("PLAYER HAS CHOOSED NO PLAY OPTION");
                 }
             }
-            if (player_position == 0 && die == 6) ;
-            {
+            if (player_position == 0 && die == 6);{
                 player_position = 1;
                 present_position = 1;
                 System.out.println("PLAYER GOT " + die + "AND ENTERED INTO THE GAME");
             }
-            if (present_position >= 100) ;
-            {
+            if (present_position >= 100);{
                 System.out.println("PLAYER REACHED " + present_position + "AND WON GAME");
                 return;
             }
