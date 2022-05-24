ab26f49d7033757877df5c9d48b2760d34ce89f5
MANOJ-51 <m4anoj51@gmail.com>
2022-05-24 17:15:46 +0530

    added:player position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,7 @@
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("WELCOME TO SNAKE & LADDER");
+        int player_position=0;
+        System.out.println("PLAYER POSITION AT START:" + player_position);
     }
 }
