a9233e3af00c7afe9c720f5978c2614dec204fd9
MANOJ-51 <m4anoj51@gmail.com>
2022-05-24 18:49:44 +0530

    added:uc4

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,23 +1,47 @@
 public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("WELCOME TO SNAKE & LADDER");
-        int player_position=0;
-        final int Ladder=1;
-        final int snake =2;
-        System.out.println("PLAYER POSITION AT START:" + player_position);
-        int die =(int) Math.floor(Math.random()*6+1);
-        int options=(int)Math.floor(Math.random()*3);
-        switch (options){
-            case Ladder:
-                player_position += die;
-                System.out.println("PLAYER CLIMBED LADDER WITH :" +die);
-                break;
-            case snake:
-                player_position -= die;
-                System.out.println("PLAYER WAS BITE BY A SNAKE WITH:"+die);
-                break;
-            default:
-                System.out.println("PLAYER HAS CHOOSED NO PLAY OPTION");
+        int player_position = 0;
+        int present_position = 0;
+        int die_roll = 0;
+        final int Ladder = 1;
+        final int snake = 2;
+        while (true) {
+            die_roll++;
+            int die = (int) Math.floor(Math.random() * 6 + 1);
+            System.out.println("DIE ROLLED BY PLAYER AND VALUE IS:" + die);
+            if (player_position == 1) {
+                int options = (int) Math.floor(Math.random() * 3);
+                switch (options) {
+                    case Ladder:
+                        present_position += die;
+                        System.out.println("PLAYER CLIMBED LADDER WITH :" + die);
+                        break;
+                    case snake:
+                        present_position -= die;
+                        if (present_position < 0) {
+                            player_position = 0;
+                            present_position = 0;
+                            System.out.println("PLAYER GOT ELIMINATED. PLAYER PRESENT POSITION :" + present_position);
+                        }
+                        System.out.println("PLAYER WAS BITE BY A SNAKE WITH:" + die);
+                        break;
+                    default:
+                        System.out.println("PLAYER HAS CHOOSED NO PLAY OPTION");
+                }
+            }
+            if (player_position == 0 && die == 6) ;
+            {
+                player_position = 1;
+                present_position = 1;
+                System.out.println("PLAYER GOT " + die + "AND ENTERED INTO THE GAME");
+            }
+            if (present_position >= 100) ;
+            {
+                System.out.println("PLAYER REACHED " + present_position + "AND WON GAME");
+                return;
+            }
         }
     }
 }
+
