4bc8e995db5ff357244a526724a091bab0778118
MANOJ-51 <m4anoj51@gmail.com>
2022-05-24 17:23:39 +0530

    added:die value

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,5 +3,7 @@ public class SnakeLadder {
         System.out.println("WELCOME TO SNAKE & LADDER");
         int player_position=0;
         System.out.println("PLAYER POSITION AT START:" + player_position);
+        int die =(int) Math.floor(Math.random()*6+1);
+        System.out.println("DIE VALUE:"+ die);
     }
 }
